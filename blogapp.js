d4862d8d82253dc7f7a949af7f4e5075190063ea
mpilo.dlamini@yahoo.com <mpilo.dlamini@yahoo.com>
2019-06-03 21:31:11 +0200

    Fixed css project can now read css file

--- blogapp.js
+++ blogapp.js
@@ -14,7 +14,8 @@ const port = 3000;
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(__dirname + "public"));
+// app.use(express.static(__dirname + "public"));
+app.use(express.static(__dirname + '/public'));
 
 let posts = [];
 
